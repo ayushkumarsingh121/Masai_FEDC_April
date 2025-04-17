ad1186af93a2bfd5b296cd78d5d151cdeca5f2b8
ayushkumarsingh121 <ayushkumarsinghrajput121@gmai.com>
2025-04-17 17:39:44 +0530

    Q2 added

---
+++ Masai School JavaScript/Class 3/Q2.js
@@ -0,0 +1,22 @@
+// Q: 12
+// Title:
+// L0 - Greet User With Prompt Input
+
+// Problem Statement:
+// Write a function called greetUser that prompts the user to enter their name using the prompt() function. If the user enters a name, greet them with the message: "Hello, <name>!". If no name is provided (i.e., the user presses "Cancel" or enters an empty string), the function should use a default name "Guest" and greet them with: "Hello, Guest!".
+
+// Requirements:
+
+// Use a function expression.
+// Ensure the function handles cases where the user cancels or leaves the input empty.
+
+//==========================================================================================
+
+let greetUser = (name) => {
+ 
+  name = prompt("Enter your name:") || "Guest"; 
+  console.log(`Hello, ${name}`);
+};
+
+greetUser("Ayush");  
+greetUser();         
